6cf8411a725cd001076c0eff2cf7e3eb411db60f
medofox123 <135126620+medofox123@users.noreply.github.com>
2024-11-11 22:43:27 +0200

    Uploaded Successfully

---
+++ plugins/fun-simi.js
@@ -0,0 +1,31 @@
+import translate from '@vitalets/google-translate-api';
+import fetch from 'node-fetch';
+const handler = async (m, {text, command, args, usedPrefix}) => {
+if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙃𝘼𝘽𝙇𝘼𝙍 𝘾𝙊𝙉𝙈𝙄𝙂𝙊\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} مرحبا Gata Bot*\n\n𝙒𝙍𝙄𝙏𝙀 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝙏𝘼𝙇𝙆 𝙏𝙊 𝙈𝙀\n\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} مرحبا بوت*`
+try {
+await conn.sendPresenceUpdate('composing', m.chat)
+let api = await fetch(`${apis}/tools/simi?text=${text}`)
+let resSimi = await api.json()
+await m.reply(resSimi.data.message)
+} catch {
+try {
+if (text.includes('مرحبا')) text = text.replace('مرحبا', 'مرحبا');
+if (text.includes('مرحبا')) text = text.replace('مرحبا', 'مرحبا');
+if (text.includes('مرحبا')) text = text.replace('مرحبا', 'مرحبا');
+const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ar&dt=t&q=' + text);
+const resu = await reis.json();
+const nama = m.pushName || '1';
+const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
+const res = await api.json();
+const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ar&dt=t&q=' + res.cnt);
+const resu2 = await reis2.json();
+m.reply(resu2[0][0][0]);
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'ar' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)
+}}}
+handler.help = ['simsimi']
+handler.tags = ['General']
+handler.command = ['bot', 'سس', 'simsimi'] 
+export default handler
