94ea74dc0bcd39e65171d57d007b485de0255be0
medofox123 <135126620+medofox123@users.noreply.github.com>
2024-11-11 22:44:02 +0200

    Uploaded Successfully

---
+++ plugins/_bienvenidos.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, participants, groupMetadata }) => {
+
+    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/admins.png'
+   
+    let text =`_👋🏻مرحبا، مرحبا بالجميع في:_\n *${groupMetadata.subject}*
+
+${groupMetadata.desc?.toString() || 'desconocido'}
+`.trim()
+const mentionedJid = groupMetadata.participants.map(v => v.id);
+    conn.sendFile(m.chat, pp, 'pp.jpg', text, null, true, { mentions: mentionedJid})
+}
+
+handler.command = ['welcome','bienvenidos','bienbenidos'] 
+handler.group = true
+handler.admin = true
+
+export default handler
