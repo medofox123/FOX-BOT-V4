f98e2cad8b5502af1c0822e0582c9d0b774f2daa
medofox123 <135126620+medofox123@users.noreply.github.com>
2024-11-11 22:42:07 +0200

    Uploaded Successfully

---
+++ plugins/random-cat.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, text }) => {
+try {
+let res = await fetch('https://cataas.com/cat')
+let img = await res.buffer()
+let caption = `𝐹𝛩𝑋 𝑊𝐴 𝐵𝛩𝑇
+`.trim()
+conn.sendFile(m.chat, img, 'cat.jpg', caption, m)
+} catch (e) {
+console.log(e)
+throw '*Error!*'
+}}
+handler.help = ['cat']
+handler.tags = ['random']
+handler.command = /^قطه$/i
+handler.fail = null
+export default handler
